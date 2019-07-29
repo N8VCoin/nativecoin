866ac5f1b22f6c0128ee2bed4e5c2aec373a5d18
dev <dev@n8vcoin.io>
2019-07-29 03:02:25 -0400

    add missing output files

---
+++ src/invalid_serials.json.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2018 The nativecoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+
+
+#ifndef nativecoin_INVALID_SERIALS_JSON_H
+#define nativecoin_INVALID_SERIALS_JSON_H
+
+#include <string>
+
+std::string LoadInvalidSerials()
+{
+    std::string str ="[]";
+    return str;
+}
+
+#endif //nativecoin_INVALID_SERIALS_JSON_H
